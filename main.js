9070231987f579b6003bf10cec598ae98222407e
iDineshRoy <dinesh.roy@hotmail.com>
2021-08-21 16:37:17 +0545

    added remove from cart button

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+          this.cart.push(id)
+      },
+      removeFromCart(id) {
+          this.cart.pop(id)
+      },
+    },
+    compute:{
+        
+    }
 })
